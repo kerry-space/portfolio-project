e3368355063b42c44fbd74edf0cb7c23c1629e91
kerry jalal <khariy34@hotmail.com>
2022-11-19 20:41:20 +0100

    upload package.js

--- src/components/navBar/NavbarMenu.jsx
+++ src/components/navBar/NavbarMenu.jsx
@@ -39,17 +39,17 @@ const NavbarMenu = () => {
         <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end '>
         <Nav className="me-auto rounded ">
             <Nav.Link id='link' href="/" className="nav-link "><HomeRoundedIcon/>Home</Nav.Link>
-            <Nav.Link id='link' href="/about" className="nav-link"><InfoRoundedIcon/>About Me</Nav.Link>
-            <Nav.Link id='link' href="/skill" className="nav-link"><CodeRoundedIcon/>Skills</Nav.Link>
-            <Nav.Link id='link' href="/portfolio" className="nav-link"><FindInPageRoundedIcon/>Portfolio</Nav.Link>
-            <Nav.Link id='link' href="/contact" className="nav-link"><ContactsRoundedIcon/>Contact</Nav.Link>
+            <Nav.Link id='link' href="/portfolio-project/about" className="nav-link"><InfoRoundedIcon/>About Me</Nav.Link>
+            <Nav.Link id='link' href="/portfolio-project/skill" className="nav-link"><CodeRoundedIcon/>Skills</Nav.Link>
+            <Nav.Link id='link' href="/portfolio-project/portfolio" className="nav-link"><FindInPageRoundedIcon/>Portfolio</Nav.Link>
+            <Nav.Link id='link' href="/portfolio-project/contact" className="nav-link"><ContactsRoundedIcon/>Contact</Nav.Link>
             
             
             {/*this would be as alternative to portfoli card, or we can show on navbar projct NavDropDown*/}
             <NavDropdown title={title} id="basic-nav-dropdown">
-              <NavDropdown.Item id='link' href="/todolist"  name="ToDoList" className="nav-link" onClick={ (e) => changeDropdownTitle(e) }>ToDoList</NavDropdown.Item>
-              <NavDropdown.Item id='link' href="/theme"   name="theme" className="nav-link theme-bg" onClick={ (e) => changeDropdownTitle(e) }>Theme</NavDropdown.Item>
-              <NavDropdown.Item id='link' href="/shoppingcard"   name="shoppingCard" className="nav-link theme-bg" onClick={ (e) => changeDropdownTitle(e) }>ShoppingCard</NavDropdown.Item>
+              <NavDropdown.Item id='link' href="/portfolio-project/todolist"  name="ToDoList" className="nav-link" onClick={ (e) => changeDropdownTitle(e) }>ToDoList</NavDropdown.Item>
+              <NavDropdown.Item id='link' href="/portfolio-project/theme"   name="theme" className="nav-link theme-bg" onClick={ (e) => changeDropdownTitle(e) }>Theme</NavDropdown.Item>
+              <NavDropdown.Item id='link' href="/portfolio-project/shoppingcard"   name="shoppingCard" className="nav-link theme-bg" onClick={ (e) => changeDropdownTitle(e) }>ShoppingCard</NavDropdown.Item>
              
             </NavDropdown>
             
